cbbdddd1cb01d589ff39c2fced5b2a01652d4c8f
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-02-14 12:53:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+var btn = document.getElementById("btn")
+var ptag = document.getElementById("num")
+var input = document.getElementById("guess").value;
+var respond = document.getElementById("respond")
 
+var diff = Number(input) - Number(ptag.value);
+if(Math.abs(diff) <=5 ){
+respond.textContent="Hot"
+}else{
+  respond.textContent="Cold"
+}
+
+
+
+function getRandomNumber(){
+var number = Math.floor(Math.random() * 40) - 20;
+ptag.textContent = number
+
+}
+btn.addEventListener("click",getRandomNumber)
